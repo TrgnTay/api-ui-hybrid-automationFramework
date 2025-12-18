e2c3776f94e0227b303f4b316fd4782ef1f601bf
T X <ttaayy100@gmail.com>
2025-12-18 16:12:20 +0100

    Move config.properties from main to test resources

---
+++ src/main/java/utils/ConfigReader.java
@@ -0,0 +1,36 @@
+package utils;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigReader {
+	
+	private static Properties properties = new Properties();
+	
+	static {
+		
+		try(InputStream is = ConfigReader.class.getClassLoader().getResourceAsStream("config.properties")){
+			
+			properties.load(is);
+			
+		}
+		 catch(Exception e) {
+			 
+			  throw new RuntimeException("Failed to load config");
+			 	 
+			 
+		 }
+			
+		
+	}
+		
+
+	public static String get(String key) {
+		
+		return properties.getProperty(key);
+		
+		
+	}
+	
+	
+}
