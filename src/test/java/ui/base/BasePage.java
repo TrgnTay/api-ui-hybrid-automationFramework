07f147c8293b5c643041e1eec25e5697e6ebc54a
T X <ttaayy100@gmail.com>
2025-12-23 16:08:25 +0100

    Add BasePage

---
+++ src/test/java/ui/base/BasePage.java
@@ -0,0 +1,46 @@
+package ui.base;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public abstract class BasePage {
+	
+	protected WebDriver driver;
+	protected WebDriverWait wait;
+	
+	
+	protected BasePage(WebDriver driver) {
+	
+	this.driver= driver;
+	this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		
+	}
+	
+	
+	protected WebElement waitForVisible(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    protected void click(By locator) {
+        waitForVisible(locator).click();
+    }
+
+    protected void type(By locator, String text) {
+        WebElement element = waitForVisible(locator);
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    protected String getText(By locator) {
+        return waitForVisible(locator).getText();
+    }
+
+    protected boolean isDisplayed(By locator) {
+        return waitForVisible(locator).isDisplayed();
+    }
+}
